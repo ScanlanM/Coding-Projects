966ccbab254be265005b1f5dcf766ed00027db06
Matthew Scanlan <mjkenney91@gmail.com>
2025-08-02 16:03:09 -0700

    Update app.js

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,29 +1,40 @@
-    function retainInput() {
-        var calculatorDisplayValue = document.getElementById("inputBox").textContent
-    }
+function retainInput() {
+    var calculatorDisplayValue = document.getElementById("inputBox").textContent
+}
 
-    function buttonClick(key){
+function buttonClick(key){
 
-        usrInput = document.getElementById('inputBox').textContent;
+    usrInput = document.getElementById('inputBox').textContent;
 
-        if (key=="C"){
-            document.getElementById("inputBox").textContent = ""
-            
-        } else if (key=="="){
+    if (key=="C"){
+        document.getElementById("inputBox").textContent = ""
 
-            try {
-                content = eval(document.getElementById("inputBox").textContent)
-                document.getElementById("inputBox").textContent = content;
-            }
-            catch(err) {
-                document.getElementById("errorOutput").innerHTML = err.message;
-            }
-            
-        } else {
-            console.log(key,usrInput);
-            let content = usrInput.concat("",key);
-            document.getElementById("inputBox").textContent = content;
+    } else if (key=="="){
+
+        try {
+            stringtoEval = parenHandler(document.getElementById("inputBox").textContent)
+            results = eval(stringtoEval)
+            document.getElementById("inputBox").textContent = results
         }
+        catch(err) {
+            document.getElementById("errorOutput").innerHTML = err.message;
+        }
+        
+    } else {
+        console.log(key,usrInput);
+        let content = usrInput.concat("",key);
+        document.getElementById("inputBox").textContent = content;
     }
+    
+}
 
+function parenHandler(usrInput){
+    /*need to handle d paren and paren d since eval doesn't recognize this as implied multiplication*/
+    rexDParen = "(\d) \(";
+    rexParenD = "\) (\d)";
+    stringtoEval = usrInput;
+    stringtoEval = stringtoEval.replaceAll(/(\d) \(/g, "$1*\(");
+    stringtoEval = stringtoEval.replaceAll(/\) (\d)/g,")*$1");
+    return stringtoEval
 
+}
