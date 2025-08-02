6497963c362a4271450073b02d5f0fafd2d34c0f
Matthew Scanlan <mjkenney91@gmail.com>
2025-08-02 15:00:59 -0700

    Refactoring and added error handling
    Moving JS to a separate file
    added error handling

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,3 +1,29 @@
+    function retainInput() {
+        var calculatorDisplayValue = document.getElementById("inputBox").textContent
+    }
 
+    function buttonClick(key){
+
+        usrInput = document.getElementById('inputBox').textContent;
+
+        if (key=="C"){
+            document.getElementById("inputBox").textContent = ""
+            
+        } else if (key=="="){
+
+            try {
+                content = eval(document.getElementById("inputBox").textContent)
+                document.getElementById("inputBox").textContent = content;
+            }
+            catch(err) {
+                document.getElementById("errorOutput").innerHTML = err.message;
+            }
+            
+        } else {
+            console.log(key,usrInput);
+            let content = usrInput.concat("",key);
+            document.getElementById("inputBox").textContent = content;
+        }
+    }
 
 
