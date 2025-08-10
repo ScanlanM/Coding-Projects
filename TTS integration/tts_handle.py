42fe549385ff96503e9ac32582d5dc7848e8472d
Matthew Scanlan <mjkenney91@gmail.com>
2025-08-10 15:52:40 -0700

    MVP for a TTS program
    tts_handler.py implements ElevenLabs's python SDK to generate speech given a string.
    
    In future updates, will build in chat intake for Twitch.tv streaming chat to tts. Intended features include voice choice via chat, default voices and more.
    Possibly build in an integration for OBS.

---
+++ TTS integration/tts_handle.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
+import os
+
+load_dotenv()
+
+api_key=os.getenv("ELEVENLABS_API_KEY")
+
+client = ElevenLabs(
+    api_key=api_key
+)
+
+textToRead="Place holder text, to be updated beyond MVP"
+
+#response = client.voices.search()
+#print(response.voices)
+
+voice_choice = "JBFqnCBsd6RMkjVDRZzb"
+
+audio = client.text_to_speech.convert(
+
+    text=textToRead, #pull in text
+    voice_id= voice_choice,
+    model_id="eleven_multilingual_v2",
+    output_format="mp3_22050_32"
+
+)
+
+play(audio)
